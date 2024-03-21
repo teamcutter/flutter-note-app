7d06f3d3950564c4608ef338672bee967c771501
teamcutter <ruslan.sibgatulin05@gmail.com>
2024-03-21 22:39:19 +0500

    minor fix

--- lib/pages/update_note_page.dart
+++ lib/pages/update_note_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/models/note_database.dart';
